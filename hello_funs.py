19b06ddf5c19fd5491175be21eb06ca2caffa517
Yann CARDAILLAC <ycardaillac.ext@esme-solutions.com>
2024-01-10 11:42:25 +0100

    say happy new year

--- hello_funs.py
+++ hello_funs.py
@@ -1,6 +1,13 @@
 
+def say_something_to_someone(what, to_who):
+	print(what  + " " + to_who)
+
 def say_hello(to_who):
-	print("hello " + to_who)
+	say_something_to_someone("Hello", to_who)
 
 def say_hello_world():
 	say_hello("World")
+
+def say_happy_new_year(to_who):
+	say_something_to_someone("Happy new year", to_who)
+
--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,4 @@ import hello_funs
 
 hello_funs.say_hello("Bob")
 hello_funs.say_hello_world()
+hello_funs.say_happy_new_year("Bob!")
