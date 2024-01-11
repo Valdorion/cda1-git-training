67c26d640ebe94488ce1cb7e0e3ed61b050eafe9
Yann CARDAILLAC <ycardaillac.ext@esme-solutions.com>
2024-01-11 11:46:48 +0100

    make code testable

--- hello_funs.py
+++ hello_funs.py
@@ -1,16 +1,16 @@
 """ few functions to say stuff """
 
 def say_something_to_someone(what, to_who):
-	print(what  + " " + to_who)
+	return what  + " " + to_who
 
 def say_hello(to_who):
-	say_something_to_someone("Hello", to_who)
+	return say_something_to_someone("Hello", to_who)
 
 def say_hello_world():
-	say_hello("World")
+	return say_hello("World")
 
 def say_happy_birthday(to_who):
-	say_something_to_someone("Happy Birthday ", to_who)
+	return say_something_to_someone("Happy Birthday ", to_who)
 
 def say_happy_new_year(to_who):
-	say_something_to_someone("Happy new year", to_who)
+	return say_something_to_someone("Happy new year", to_who)
--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@
 
 import hello_funs
 
-hello_funs.say_hello("Bob")
-hello_funs.say_hello_world()
-hello_funs.say_happy_birthday("Bob!")
-hello_funs.say_happy_new_year("Bob!")
+print(hello_funs.say_hello("Bob"))
+print(hello_funs.say_hello_world())
+print(hello_funs.say_happy_birthday("Bob!"))
+print(hello_funs.say_happy_new_year("Bob!"))
